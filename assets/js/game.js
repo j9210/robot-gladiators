ae29e9d161820016909d86c21eb62eac788add44
j9210 <71297542+j9210@users.noreply.github.com>
2020-10-08 17:36:50 -0500

    the for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,23 @@
-// this creates a function named "fight"
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//      * Fight all enemy robots
+//      * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
 
+//Player Variables
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-// You can also log multiple values at once i.e.
-console.log (playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+// Enemy Variables
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+
+// "Fight" Function
+var fight = function (enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -63,10 +69,7 @@ var fight = function () {
         window.alert("You need to pick a valid option. Try again!");
     }
 }
-    
 
-
-
-
-
-fight ();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
